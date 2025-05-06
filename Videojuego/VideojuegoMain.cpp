cbe27cf20dcc0621666351bf7f2ea06e737d3a37
rovalemi <rovaalemi.es@gmail.com>
2025-05-07 01:51:13 +0200

    cuarto-proyecto
    Subida de cuarto proyecto

---
+++ Videojuego/VideojuegoMain.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+/* PRIMERA PRUEBA
+* 
+int heroHP1 = 100;
+int heroA1 = 15;
+
+int enemyHP1 = 80;
+int enemyA1 = 30;
+
+void gameAttack() {
+	while (heroHP1 > 0 && enemyHP1 > 0) {
+		enemyHP1 -= heroA1;
+		cout << "El heroe ataca y causa " << heroA1 << " de dano. Enemigo HP: " << enemyHP1 << ".\n";
+		if (enemyHP1 <= 0) break;
+
+		heroHP1 -= enemyA1;
+		cout << "El enemigo ataca y causa " << enemyA1 << " de dano. Heroe HP: " << heroHP1 << ".\n";
+	}
+}
+
+void printStatus() {
+	if (heroHP1 > 0) {
+		cout << "El heroe ha ganado el combate!\n";
+	}
+	else {
+		cout << "El enemigo ha ganado el combate!\n";
+	}
+}
+
+int main() {
+	cout << "Let's start!\n";
+	gameAttack();
+	printStatus();
+	system("pause");
+	return 0;
+}
+*/
+
+
+
